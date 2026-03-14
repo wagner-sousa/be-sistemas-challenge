9b3b72341ba158417a0d3e07023d8fd99a9a6d6c
Wagner Sousa <wagner.fernandes.sousa@outlook.com>
2026-03-14 14:59:10 +0000

    build: :technologist: Adiciona configuração do servidor no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
