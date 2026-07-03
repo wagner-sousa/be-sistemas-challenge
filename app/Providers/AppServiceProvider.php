19e8c23ed5c8d3bbbf68bfafa2710f4c8ec3c3e8
Wagner Sousa <wagner.fernandes.sousa@outlook.com>
2026-07-02 22:42:35 -0300

    chore: Atualiza dependências e configurações
    - Adiciona Laravel Sanctum para autenticação API
    - Configura CORS para API
    - Atualiza configurações do Inertia e Vite
    - Ajusta docker-compose e providers
    - Atualiza package.json e composer.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        // ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+        //     return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        // });
     }
 }
